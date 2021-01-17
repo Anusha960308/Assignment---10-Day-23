658b1811f271ba03216956d7e4118eca835327fe
Anusha960308 <70646414+Anusha960308@users.noreply.github.com>
2021-01-17 17:33:52 +0530

    Add files via upload

---
+++ Assignment - 10 Day 23.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+* directory ----os.walk()
+
+
+# In[5]:
+
+
+list1 = [c1,d1,e1]
+dict1 = []
+
+def_funct(drive)
+resp = os.walk(drive)
+
+
+dict1[file] = path
+
+
+for drive in list1:
+    th1 = Thread(target = funct(drive))
+    th1.start[]
+    
+    
+
+
+# In[ ]:
+
+
+
+
